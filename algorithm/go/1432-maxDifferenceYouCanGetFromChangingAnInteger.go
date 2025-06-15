16788da77b85bf578311a028b56cb1707587c75e
nekloyh <nlminky04@gmail.com>
2025-06-15 22:55:19 +0700

    implement 1432

---
+++ algorithm/go/1432-maxDifferenceYouCanGetFromChangingAnInteger.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func maxDiff(num int) int {
+	numStr := strconv.Itoa(num)
+	uniqueDigits := make(map[byte]bool)
+	for i := 0; i < len(numStr); i++ {
+		uniqueDigits[numStr[i]] = true
+	}
+
+	maxVal := num
+	minVal := num
+
+	for digit := range uniqueDigits {
+		for d := byte('0'); d <= '9'; d++ {
+			if numStr[0] == digit && d == '0' {
+				continue
+			}
+
+			newStr := strings.ReplaceAll(numStr, string(digit), string(d))
+			newNum, err := strconv.Atoi(newStr)
+			if err != nil {
+				continue
+			}
+			if newNum > maxVal {
+				maxVal = newNum
+			}
+			if newNum < minVal {
+				minVal = newNum
+			}
+		}
+	}
+
+	return maxVal - minVal
+}
+
+func main() {
+	num := 555
+	fmt.Println(maxDiff(num)) 
+}
