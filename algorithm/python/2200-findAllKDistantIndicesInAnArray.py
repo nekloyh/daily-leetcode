d4f4ce5f3c71491488d4497889a7e4bbfff571e2
nekloyh <nlminky04@gmail.com>
2025-06-24 23:10:12 +0700

    implement 2200

---
+++ algorithm/python/2200-findAllKDistantIndicesInAnArray.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        res = []
+        r = 0
+        n = len(nums)
+        for j in range(n):
+            if nums[j] == key:
+                l = max(r, j - k)
+                r = min(n - 1, j + k) + 1
+                for i in range(l, r):
+                    res.append(i)
+        return res
+            
+def main():
+    sol = Solution()
+    nums = [3, 4, 9, 1, 3, 9, 5]
+    key = 9
+    k = 1
+    print(sol.findKDistantIndices(nums, key, k))
+
+if __name__ == "__main__":
+    main()
