08d69dfe7337e4358710155c13c655e731af2e4e
nekloyh <nlminky04@gmail.com>
2025-07-10 18:41:54 +0700

    implement 3440

---
+++ algorithm/python/3440-rescheduleMeetingsForMaximumFreeTime2.py
@@ -0,0 +1,33 @@
+from typing import List
+
+class Solution:
+    def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
+        gap = [startTime[0]]
+        for i in range(1, len(startTime)):
+            gap.append(startTime[i] - endTime[i - 1])
+        gap.append(eventTime - endTime[-1])
+
+        largestRight = [0] * len(gap)
+        for i in range(len(gap) - 2, -1, -1):
+            largestRight[i] = max(largestRight[i + 1], gap[i + 1])
+
+        ans = 0
+        largestLeft = 0
+        for i in range(1, len(gap)):
+            curGap = endTime[i - 1] - startTime[i - 1]
+            if curGap <= max(largestLeft, largestRight[i]):
+                ans = max(ans, gap[i - 1] + gap[i] + curGap)
+            ans = max(ans, gap[i - 1] + gap[i])
+            largestLeft = max(largestLeft, gap[i - 1])
+
+        return ans
+    
+def main():
+    sol = Solution()
+    eventTime = 5
+    startTime = [1, 3]
+    endTime = [2, 5]
+    print(sol.maxFreeTime(eventTime, startTime, endTime))
+    
+if __name__ == "__main__":
+    main()
